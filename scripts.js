7f8492ed8b7b5f701be9aa3f51774a0ca661e9c4
snehalsj021 <78582947+snehalsj021@users.noreply.github.com>
2021-02-05 12:03:22 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	let i = 1;
-	let total = 5;
+	let total = 3;
 	// $("#image-container").css("background-image", "url(" + images[0] + ")");
 	setInterval(function(){ changeImage(i++%3); }, 5000);
 });
